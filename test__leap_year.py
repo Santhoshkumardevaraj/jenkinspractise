7e9f71dbd1d2d8a9ebc90960adc8b5e0449d294c
Santhosh Kumar Devaraj <39290866+Santhoshkumardevaraj@users.noreply.github.com>
2026-01-17 16:59:58 +0530

    Create test__leap_year.py

---
+++ test__leap_year.py
@@ -0,0 +1,7 @@
+from leap_year import is_leap_year
+
+def test_leap_year():
+    assert is_leap_year(2024) is True
+    assert is_leap_year(1900) is False
+    assert is_leap_year(2000) is True
+    assert is_leap_year(2023) is False
